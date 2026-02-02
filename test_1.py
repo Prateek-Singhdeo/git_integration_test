87519b2254e706e456074e8347eaa6e01a21821e
Prateek-Singhdeo <prateeksinghdeo@gmail.com>
2026-02-02 09:13:02 +0000

    testing...

--- test_1.py
+++ test_1.py
@@ -31,7 +31,7 @@ CONFIG = {
 
 def log_debug(message: str):
     if CONFIG["log_level"] == "DEBUG":
-        print(f"[DEBUG] {message}1")
+        print(f"[DEBUG] {message}2")
 
 def log_info(message: str):
     print(f"[INFO] {message}")
