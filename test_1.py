353525e0126f3eec7bfcd184d02dd9fd6aa44cc0
Prateek Singhdeo <psinghde@dhcp-10-48-43-148.vpn.oracle.com>
2026-02-04 18:12:26 +0530

    test

--- test_1.py
+++ test_1.py
@@ -31,7 +31,7 @@ CONFIG = {
 
 def log_debug(message: str):
     if CONFIG["log_level"] == "DEBUG":
-        print(f"[DEBUG] {message}6")
+        print(f"[DEBUG] {message}8")
 
 def log_info(message: str):
     print(f"[INFO] {message}")
